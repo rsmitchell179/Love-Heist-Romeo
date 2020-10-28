f1f61830101c9fea75cf52f4441d92426dbc44e5
Ryan Mitchell <rsmitche@ucsc.edu>
2020-10-28 14:15:10 -0700

    Title Screen added but buggy

---
+++ No Hesitation Move!/Assets/Scripts/mainMenuScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class mainMenuScript : MonoBehaviour {
+	public string newGameScene;
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    public void StartGame() {
+        Debug.Log("clicked");
+    	SceneManager.LoadScene(newGameScene);
+    }
+}
