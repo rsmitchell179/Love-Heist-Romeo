be6ea6cdea95677318d48cf1782fba1a26925438
thealexanderkuang <49510751+thealexanderkuang@users.noreply.github.com>
2020-11-19 02:47:33 -0800

    Implemented Outro Slideshow
    - Created new Outro scene
    - Added slideshow script
    - Created new door that changes to Outro scene
    - Looped outro to main menu

---
+++ No Hesitation Move!/Assets/Scripts/OutroDoorScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OutroDoorScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // public string destinationScene;
+    // public Vector2 destinationCoords;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            globals.putPlayer = true;
+            // globals.destPos = destinationCoords;
+            SceneManager.LoadScene("OutroSlideShow");
+
+        }
+    }
+}
---
+++ No Hesitation Move!/Assets/Scripts/OutroSlideShowScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Versioning;
+using System.Security.AccessControl;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OutroSlideShowScript : MonoBehaviour
+{
+    // what room we move to after slideshow
+    public string nextRoom;
+
+    // sprites for intro frames
+    public Sprite[] frame;
+
+    // object's sprite renderer
+    SpriteRenderer spr;
+
+    // what frame are we on?
+    private int spriteOn = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get the sprite object, so we can change the frame
+        spr = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    void OnMouseDown()
+    {
+        // if we still have frames to go, move to next frame
+        if (spriteOn < frame.Length-1)
+        {
+            spriteOn++;
+            spr.sprite = frame[spriteOn];
+        // otherwise, go to next room
+        } else
+        {
+            SceneManager.LoadScene(nextRoom);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            SceneManager.LoadScene(nextRoom);
+        }
+    }
+}
