998d36e86ef95248ab08204c133c0d28f21f3761
thealexanderkuang <49510751+thealexanderkuang@users.noreply.github.com>
2021-01-23 05:04:58 -0800

    appquit
    -added ability to exit game with 'ESC' key

---
+++ No Hesitation Move!/Assets/Scripts/DD_appquit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DD_appquit : MonoBehaviour
+{
+
+	public GameObject appquit_script;
+
+	void Awake()
+	{
+
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DontDestroyOnLoad(this.appquit_script);
+    }
+}
