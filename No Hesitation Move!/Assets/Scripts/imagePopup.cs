a97da1ae49832f20e7fbe3036a7b5dc016d24b10
thealexanderkuang <49510751+thealexanderkuang@users.noreply.github.com>
2021-01-30 03:09:18 -0800

    added letter popup after budelia dialogue
    - press tab after letter popup to close image

---
+++ No Hesitation Move!/Assets/Scripts/imagePopup.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Yarn.Unity;
+
+public class imagePopup : MonoBehaviour
+{
+
+	public Image letter;
+	public DialogueRunner dialogueRunner;
+
+	public void Awake() {
+
+        // Create a new command called 'custom_wait', which waits for one second.
+        dialogueRunner.AddCommandHandler(
+            "img_showup",
+            img_showup
+        );
+    }
+
+	void Start()
+	{
+		letter.enabled = false;
+		Debug.Log("here");
+	}
+
+	void Update()
+	{
+
+	}
+
+	public void img_showup(string[] param, System.Action onComplete)
+	{
+		Debug.Log("before letter enabled");
+		letter.enabled = true;
+		Debug.Log("after letter enabled");
+
+		StartCoroutine(close_pop(onComplete));
+		// if(Input.GetKey(KeyCode.Tab)){
+		// 	Debug.Log("keycode Tab");
+		// 	letter.enabled = false;
+		// 	onComplete();
+		// }
+	}
+
+	public IEnumerator close_pop(System.Action onComplete){
+		// yield return null;
+		while(true){
+			if(Input.GetKey(KeyCode.Tab)){
+				Debug.Log("keycode Tab");
+				letter.enabled = false;
+				onComplete();
+				yield break;
+			}
+			yield return null;
+		}
+	}
+}
