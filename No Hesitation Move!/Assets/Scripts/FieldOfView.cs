c7566d2809770108b9ee44dba15bf1319efd2788
KanWen-G <53582578+KanWen-G@users.noreply.github.com>
2021-02-13 16:59:01 -0800

    install URP

---
+++ No Hesitation Move!/Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    [SerializeField] private LayerMask layerMask;
+    private Mesh mesh;
+    public float fov;
+    public float viewDistance;
+    private Vector3 origin;
+    public float startingAngle;
+
+    private Camera mainCam;
+
+    private void Awake()
+    {
+        mainCam = Camera.main;
+        
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        fov = 90f;
+        viewDistance = 10f;
+        origin = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        //SetAimDirection(GetMouseWorldPosition() - transform.position.normalized);
+    }
+
+    private void LateUpdate()
+    {
+        int rayCount = 200;
+        float angle = startingAngle;
+        float angleIncrease = fov / rayCount;
+
+        Vector3[] vertices = new Vector3[rayCount + 2];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[rayCount * 3];
+
+        vertices[0] = origin;
+
+        int vertexIndex = 1;
+        int triangleIndex = 0;
+        for (int i = 0; i <= rayCount; i++)
+        {
+            Vector3 vertex;
+            RaycastHit2D hit = Physics2D.Raycast(origin, GetVectorFromAngle(angle), viewDistance, layerMask);
+            if (hit.collider == null)
+            {
+                vertex = origin + GetVectorFromAngle(angle) * viewDistance;
+            }
+            else
+            {
+                vertex = hit.point;
+            }
+            vertices[vertexIndex] = vertex;
+
+            if(i > 0)
+            {
+                triangles[triangleIndex] = 0; 
+                triangles[triangleIndex + 1] = vertexIndex - 1; 
+                triangles[triangleIndex + 2] = vertexIndex;
+
+                triangleIndex += 3;
+            }
+            
+            vertexIndex++;
+            angle -= angleIncrease;
+        }
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+        mesh.bounds = new Bounds(origin, Vector3.one * 1000f);
+    }
+
+    public void SetOrigin(Vector3 origin) {
+        this.origin = origin;
+    }
+
+    public void SetAimDirection(Vector3 aimDirection) {
+        startingAngle = GetAngleFromVectorFloat(aimDirection) + fov / 2f;
+    }
+
+    public void SetFoV(float fov) {
+        this.fov = fov;
+    }
+
+    public void SetViewDistance(float viewDistance) {
+        this.viewDistance = viewDistance;
+    }
+
+    private Vector3 GetVectorFromAngle(float angle)
+    {
+        float angleRad = angle * Mathf.PI / 180f;
+        return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+    }
+
+    private float GetAngleFromVectorFloat(Vector3 dir)
+    {
+        dir = dir.normalized;
+        float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        if (n < 0) n += 360;
+        return n;
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        var v3 = Input.mousePosition;
+        v3.z = 10f;
+        v3 = mainCam.ScreenToWorldPoint(v3);
+        return v3;
+    }
+}
---
+++ No Hesitation Move!/Assets/Scripts/Patrol.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public float speed;
+    public Transform[] moveSpots;
+    private int randomSpot;
+    public float startWaitTime;
+    public float waitTime;
+    [SerializeField] private FieldOfView fieldOfView;
+    private Vector3 lastMoveDir;
+    private float viewDistance;
+    private float fov;
+    private GameObject player;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        fov = fieldOfView.fov;
+        viewDistance = fieldOfView.viewDistance;
+        waitTime = startWaitTime;
+        randomSpot = Random.Range(0,moveSpots.Length);
+        lastMoveDir = new Vector3(Random.Range(-20, 20), Random.Range(-20, 20), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FindTargetPlayer();
+        fieldOfView.SetOrigin(transform.position);
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f){
+            if(waitTime <= 0){
+                lastMoveDir = new Vector3(Random.Range(-20, 20), Random.Range(-20, 20), 0);
+                randomSpot = Random.Range(0,moveSpots.Length);
+                waitTime = startWaitTime;
+                fieldOfView.SetAimDirection(lastMoveDir);
+            } else {
+                waitTime -= Time.deltaTime;
+            }
+        }
+
+
+    }
+
+   private void FindTargetPlayer(){
+        if (Vector3.Distance(GetPosition(), player.transform.position) < viewDistance) {
+            // Player inside viewDistance
+            Vector3 dirToPlayer = (player.transform.position - GetPosition()).normalized;
+            if (Vector3.Angle(GetAimDir(), dirToPlayer) < fov / 2f) {
+                // Player inside Field of View
+                RaycastHit2D raycastHit2D = Physics2D.Raycast(GetPosition(), dirToPlayer, viewDistance);
+                if (raycastHit2D.collider != null) {
+                    // Hit something
+                    if (raycastHit2D.collider.gameObject.GetComponent<playerMovement>() != null) {
+                        // Hit Player
+                        AttackingPlayer();
+                    } else {
+                        // Hit something else
+                    }
+                }
+            }
+        }
+    }
+
+    public void AttackingPlayer() {
+        Debug.Log("Hello:  I see you");
+    }
+
+    public Vector3 GetPosition() {
+        return transform.position;
+    }
+
+    public Vector3 GetAimDir() {
+        return lastMoveDir;
+    }
+
+}
