10e9cd8bc8c15bb59395db9483f553e10a259e1a
mplaurence001 <mlaurence001@gmail.com>
2021-02-26 18:09:57 -0800

    Added Doughboy Sounds
    +Added Doughboy Sounds
    +Lowered most sound volumes
    +Chaged reverb in JS Path and Town
    +Added Cave Sounds to JS Mini Game
    +Changed Fancy Town Entrance effect

---
+++ No Hesitation Move!/Assets/Scripts/Sound/DoughboyPlaySound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoughboyPlaySound : MonoBehaviour
+{
+    AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+         source = GetComponent<AudioSource>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+    	if(other.gameObject.tag == "Player")
+    	{
+            source.Play();
+    	}
+    }
+
+}
