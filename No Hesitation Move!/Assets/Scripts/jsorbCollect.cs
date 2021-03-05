a2ace6db2cf8fd4d48af64e0d46001bacea97ee9
KanWen-G <53582578+KanWen-G@users.noreply.github.com>
2021-03-04 22:06:13 -0800

    fade when collected orb

--- No Hesitation Move!/Assets/Scripts/jsorbCollect.cs
+++ No Hesitation Move!/Assets/Scripts/jsorbCollect.cs
@@ -10,6 +10,7 @@ public class jsorbCollect : MonoBehaviour
     public GameObject owl;
     public GameObject romeo;
     public YarnProgram scriptToLoad;
+    public Animator animatior;
 
     private string talktonode;
     private bool hasCollect;
@@ -40,14 +41,22 @@ public class jsorbCollect : MonoBehaviour
     	if(playerMovement.hasJSorb == true){
             hasCollect =true;
         	// Debug.Log("yes, it's true, all of it");
+            animatior.SetTrigger("FadeOut");
+        }
 
+
+    }
+    
+    public void OnFadeComplete(){
+        if(hasCollect == true) { 
+            animatior.SetTrigger("FadeIn");           
             owl.GetComponent<Patrol>().enabled = false;
             owl.transform.position = new Vector3(18.5f, 6f, 0f);
             romeo.transform.position = new Vector3(16.5f, 5f, 0f);
 
             diaRun.Add(scriptToLoad);
             diaRun.StartDialogue(talktonode);
-            // StartDialogue();
-        }
+            // StartDialogue();}
     }
+}
 }
