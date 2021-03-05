c25d7d70be5df464c5d655ec94b879b7518814a3
thealexanderkuang <49510751+thealexanderkuang@users.noreply.github.com>
2021-03-04 21:35:59 -0800

    finished minigame complete conditions
    cool
    (also, ryan, i underestimated the orb script, good job)

--- No Hesitation Move!/Assets/Scripts/jsorbCollect.cs
+++ No Hesitation Move!/Assets/Scripts/jsorbCollect.cs
@@ -1,17 +1,30 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Yarn.Unity;
 
 
 public class jsorbCollect : MonoBehaviour
 {
     public GameObject owl;
     public GameObject romeo;
+    public YarnProgram scriptToLoad;
+
+    private string talktonode;
     private bool hasCollect;
+    private DialogueRunner diaRun = null;
     // Start is called before the first frame update
     void Start()
     {
         hasCollect =false;
+        try{
+            diaRun = FindObjectOfType<DialogueRunner>();
+            }catch(NullReferenceException){
+                Debug.Log("got nullexceptionerror in SpaceContinue, don't worry about it for now");
+            }
+
+        talktonode = "Start";
     }
 
     // Update is called once per frame
@@ -27,15 +40,14 @@ public class jsorbCollect : MonoBehaviour
     	if(playerMovement.hasJSorb == true){
             hasCollect =true;
         	// Debug.Log("yes, it's true, all of it");
+
             owl.GetComponent<Patrol>().enabled = false;
             owl.transform.position = new Vector3(18.5f, 6f, 0f);
             romeo.transform.position = new Vector3(16.5f, 5f, 0f);
+
+            diaRun.Add(scriptToLoad);
+            diaRun.StartDialogue(talktonode);
+            // StartDialogue();
         }
     }
-
-    // public void grifton_dialogue(){
-    //     if(hasCollect == true){
-            
-    //     }
-    // }
 }
