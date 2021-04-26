bac758b52c8f65ea6d6ed9e5508560122c95e361
thealexanderkuang <49510751+thealexanderkuang@users.noreply.github.com>
2021-04-26 05:51:38 -0700

    added space press to cutscene

---
+++ No Hesitation Move!/Assets/Scripts/CutsceneSpace.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CutsceneSpace : MonoBehaviour
+{
+
+	private float wait_time = 4f;
+
+	public Image space_press;
+
+	public bool is_pressed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	space_press.enabled = false;
+    	start_space();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+        	set_is_pressed();
+        	space_press.enabled = false;
+        	StartCoroutine(wait_five());
+        }
+
+        if(is_pressed = true)
+        {
+        	wait_time = 8f;
+        }
+    }
+
+    void set_is_pressed()
+    {
+    	if(is_pressed == false)
+    	{
+    		is_pressed = true;
+    	}
+    }
+
+    void start_space()
+    {
+    	StartCoroutine(wait_five());
+    }
+
+    IEnumerator wait_five()
+    {
+    	yield return new WaitForSecondsRealtime(wait_time);
+    	space_press.enabled = true;
+    }
+}
