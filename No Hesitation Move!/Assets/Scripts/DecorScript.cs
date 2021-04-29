f2ea5573d119024efef6a531c61d06833e65be45
thealexanderkuang <49510751+thealexanderkuang@users.noreply.github.com>
2021-04-29 04:28:06 -0700

    added paintings to river of paint

---
+++ No Hesitation Move!/Assets/Scripts/DecorScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecorScript : MonoBehaviour
+{
+
+	public string behind_layer = "Environment_Behind";
+	public string above_layer = "Environment_Above";
+
+	public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.transform.position.y > player.transform.position.y)
+        {
+        	this.GetComponent<SpriteRenderer>().sortingLayerName = behind_layer;
+        }
+        else
+        {
+        	this.GetComponent<SpriteRenderer>().sortingLayerName = above_layer;
+        }
+    }
+}
