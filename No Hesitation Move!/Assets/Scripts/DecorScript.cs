720dcf345340cdb3b10dfabee3739a12b251525b
thealexanderkuang <49510751+thealexanderkuang@users.noreply.github.com>
2021-04-29 19:00:08 -0700

    adjusted code a bit

--- No Hesitation Move!/Assets/Scripts/DecorScript.cs
+++ No Hesitation Move!/Assets/Scripts/DecorScript.cs
@@ -13,6 +13,7 @@ public class DecorScript : MonoBehaviour
 
 	public Color new_color;
 
+	public Vector3 player_pos;
 	public float dist_is;
 
     // Start is called before the first frame update
@@ -20,6 +21,7 @@ public class DecorScript : MonoBehaviour
     {
     	new_color = this.GetComponent<SpriteRenderer>().color;
         player = GameObject.FindWithTag("Player");
+        dist_is = 0.85f;
     }
 
     // Update is called once per frame
@@ -35,8 +37,18 @@ public class DecorScript : MonoBehaviour
         }
 
         float dist = Vector3.Distance(player.transform.position, this.transform.position);
+        player_pos = player.transform.position;
 
         if(dist < dist_is && (this.GetComponent<SpriteRenderer>().sortingLayerName == above_layer))
+        // (
+        // 	(
+        // 		(player_pos.y < this.GetComponent<SpriteRenderer>().bounds.max.y) &&
+        // 		(player_pos.x < this.GetComponent<SpriteRenderer>().bounds.max.x) &&
+        // 		(player_pos.y > this.GetComponent<SpriteRenderer>().bounds.min.y) &&
+        // 		(player_pos.x > this.GetComponent<SpriteRenderer>().bounds.min.x)
+        // 	)
+        // 	&& (this.GetComponent<SpriteRenderer>().sortingLayerName == above_layer)
+        // )
         {
         	new_color.a = 0.5f;
         	this.GetComponent<SpriteRenderer>().color = new_color;
