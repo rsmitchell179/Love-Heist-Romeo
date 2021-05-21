452227c0bd132bb2fcaeed35b419037b012b7d4c
Ryan Mitchell <rsmitche@ucsc.edu>
2021-05-21 16:22:33 -0700

    Connected settings to main menu and changed selected sprite
    uh huh

---
+++ No Hesitation Move!/Assets/Scripts/ExtraLetters.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtraLetters : MonoBehaviour {
+	public SpriteRenderer letter;
+	public Sprite letter_opened;
+	public Sprite letter_closed;
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+        	letter.sprite = letter_opened;
+        }
+    }
+}
--- No Hesitation Move!/Assets/Scripts/mainMenuScript.cs
+++ No Hesitation Move!/Assets/Scripts/mainMenuScript.cs
@@ -3,13 +3,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class mainMenuScript : MonoBehaviour {
 	
     public string newGameScene;
     private GameObject last_button;
-    public Canvas menuCanvas;
-    public Canvas settings;
+    public GameObject titleCloud;
+    public GameObject star1;
+    public GameObject star2;
+    public GameObject newGameButton;
+    public GameObject loadGameButton;
+    public GameObject quitGameButton;
+    public GameObject settingsButton;
+    public GameObject creditsButton;
+    public GameObject musicSlider;
+    public GameObject sfxSlider;
+    public GameObject resolutionDropdown;
+    public GameObject fullscreenToggle;
+    public GameObject backButton;
+    public Button settings_Button;
+    public Slider music_slider;
+
+    [Header("Audio Mixers")]
+    public AudioMixer music_mix;
+    public AudioMixer sfx_mix;
+
     // Start is called before the first frame update
     void Start() {
         
@@ -40,7 +60,51 @@ public class mainMenuScript : MonoBehaviour {
         Application.Quit();
     }
     public void Settings() {
-        menuCanvas.enabled = false;
-        settings.enabled = true;
+    	Cursor.visible = true;
+    	titleCloud.SetActive(false);
+    	star1.SetActive(false);
+		star2.SetActive(false);
+		newGameButton.SetActive(false);
+		loadGameButton.SetActive(false);
+		quitGameButton.SetActive(false);
+		settingsButton.SetActive(false);
+		creditsButton.SetActive(false);
+		musicSlider.SetActive(true);
+		sfxSlider.SetActive(true);
+		resolutionDropdown.SetActive(true);
+		fullscreenToggle.SetActive(true);
+		backButton.SetActive(true);
+		music_slider.Select();
+    }
+    public void backToMainMenu() {
+    	titleCloud.SetActive(true);
+    	star1.SetActive(true);
+		star2.SetActive(true);
+		newGameButton.SetActive(true);
+		loadGameButton.SetActive(true);
+		quitGameButton.SetActive(true);
+		settingsButton.SetActive(true);
+		creditsButton.SetActive(true);
+		musicSlider.SetActive(false);
+		sfxSlider.SetActive(false);
+		resolutionDropdown.SetActive(false);
+		fullscreenToggle.SetActive(false);
+		backButton.SetActive(false);
+		Cursor.visible = false;
+		settings_Button.Select();
+    }
+    public void set_music_volume(float volume)
+    {
+      if(volume <= 0){
+    	music_mix.SetFloat("music", volume);
+        }
+        
+    }
+
+    public void set_sfx_volume(float volume)
+    {
+       if(volume <= 0){
+    	sfx_mix.SetFloat("sfx", volume);
+        }
     }
 }
