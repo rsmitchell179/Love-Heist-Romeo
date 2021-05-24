7b818ac8599b0978b0243fd7f7ae4295cebd9b73
thealexanderkuang <49510751+thealexanderkuang@users.noreply.github.com>
2021-05-24 14:31:51 -0700

    added a lot of doors to reject city

---
+++ No Hesitation Move!/Assets/Scripts/RCExit.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RCExit : MonoBehaviour
+{
+
+	public SpriteRenderer door;
+	public Sprite door_opened;
+	public Sprite door_closed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+    	if(other.gameObject.tag == "Player" && GlobalVars.hasRCorb == true)
+    	{
+    		door.sprite = door_opened;
+    	}
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+    	if(other.gameObject.tag == "Player")
+    	{
+    		door.sprite = door_closed;
+    	}
+    }
+}
