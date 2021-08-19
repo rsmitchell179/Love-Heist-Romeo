12bc0f38cb56011977d681ea7b8867e2cf52f3df
thealexanderkuang <49510751+thealexanderkuang@users.noreply.github.com>
2021-08-19 05:56:58 -0700

    Got the JS Boat back path and rooftop working
    I think it looks cool, a lot of managing though

---
+++ No Hesitation Move!/Assets/Scripts/JSBoatBackPathScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JSBoatBackPathScript : MonoBehaviour
+{
+
+    public bool is_active;
+    public GameObject poker_roof;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            poker_roof.SetActive(false);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            poker_roof.SetActive(true);
+        }
+    }
+}
---
+++ No Hesitation Move!/Assets/Scripts/JSBoatLadderScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JSBoatLadderScript : MonoBehaviour
+{
+    public GameObject decor_transparency;
+    public DecorScript decor_script;
+    public Collider2D invis_wall;
+    public GameObject back_bath;
+    public GameObject roof_edge;
+
+    void Start()
+    {
+        roof_edge.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            // Debug.Log("before check is " + decor_transparency.activeInHierarchy);
+            decor_transparency.SetActive(false);
+            decor_script.enabled = false;
+            invis_wall.enabled = false;
+            back_bath.SetActive(false);
+            roof_edge.SetActive(true);
+            // Debug.Log("after check is " + decor_transparency.activeInHierarchy);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            decor_transparency.SetActive(true);
+            decor_script.enabled = true;
+            invis_wall.enabled = true;
+            back_bath.SetActive(true);
+            roof_edge.SetActive(false);
+        }
+    }
+}
