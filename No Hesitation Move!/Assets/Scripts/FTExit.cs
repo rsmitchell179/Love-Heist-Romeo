3e28523a7175ea6a2496ad1e7785c29a21694460
thealexanderkuang <49510751+thealexanderkuang@users.noreply.github.com>
2021-09-09 20:42:39 -0700

    Decor stuff
    - Replaced RC Black Market decor
    - Replaced FT Mansion door
    - Added in space press ui to jukebox

---
+++ No Hesitation Move!/Assets/Scripts/FTExit.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FTExit : MonoBehaviour
+{
+
+    public SpriteRenderer door;
+    public Sprite door_opened;
+    public Sprite door_closed;
+    public BoxCollider2D door_collider;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        if(GlobalVars.ft_orb_get == true)
+        {
+            door_collider.enabled = true;
+        }
+        else
+        {
+            door_collider.enabled = false;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player" && GlobalVars.ft_orb_get == true)
+        {
+            door.sprite = door_opened;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            door.sprite = door_closed;
+        }
+    }
+}
---
+++ No Hesitation Move!/Assets/Scripts/RCJukeboxBubble.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RCJukeboxBubble : MonoBehaviour
+{
+
+    public Image bubble;
+    public Image img_jukebox;
+    public GameObject jukebox;
+    public bool inside_collider;
+    public Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bubble.enabled = false;
+        img_jukebox.enabled = false;
+        cam = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(inside_collider == true)
+        {
+            bubble.enabled = true;
+            img_jukebox.enabled = true;
+            set_pos(bubble);
+        }
+        else
+        {
+            bubble.enabled = false;
+            img_jukebox.enabled = false;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            inside_collider = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            inside_collider = false;
+        }
+    }
+
+    void set_pos(Image bub)
+    {
+        float y_offset = jukebox.GetComponent<SpriteRenderer>().bounds.max.y + 0.4f;
+        Vector3 bub_position = new Vector3(jukebox.transform.position.x, y_offset, jukebox.transform.position.z);
+        bubble.transform.position = cam.WorldToScreenPoint(bub_position);
+    }
+}
