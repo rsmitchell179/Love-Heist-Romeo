4d150851bfd13d791ce521f680720a6492c8b88e
thealexanderkuang <49510751+thealexanderkuang@users.noreply.github.com>
2021-09-20 05:52:41 -0700

    Small Adjustments to the Decor Positioning

--- No Hesitation Move!/Assets/Scripts/Sound/DoughboyPlaySound.cs
+++ No Hesitation Move!/Assets/Scripts/Sound/DoughboyPlaySound.cs
@@ -8,20 +8,14 @@ public class DoughboyPlaySound : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-         source = GetComponent<AudioSource>();  
+        source = GetComponent<AudioSource>();  
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
 
     void OnTriggerEnter2D(Collider2D other)
     {
     	if(other.gameObject.tag == "Player")
     	{
+            Debug.Log("Playing");
             source.Play();
     	}
     }
