6cd380723b99746fdbd59d8f16fad888ad0fccd6
thealexanderkuang <49510751+thealexanderkuang@users.noreply.github.com>
2021-10-01 04:48:34 -0700

    Changed the Jukebox Tracks to 'Streaming' Load Type
    This makes it so the tracks aren't preloaded onto memory, which was causing the lag spike before the scene loaded. Will playtest this to see if the increased CPU usage is worth the exchange in load times.

---
+++ No Hesitation Move!/Assets/Scripts/LoadSceneInBackground.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneInBackground : MonoBehaviour
+{
+    public int scene_index;
+
+    void Awake()
+    {
+        Application.backgroundLoadingPriority = ThreadPriority.High;
+        StartCoroutine(load_scene_async(scene_index));
+    }
+
+    IEnumerator load_scene_async(int index)
+    {
+
+        yield return new WaitForSeconds(0.1f);
+
+        AsyncOperation async_load = SceneManager.LoadSceneAsync(index);
+        async_load.allowSceneActivation = false;
+
+        while(!async_load.isDone)
+        {
+            Debug.Log("Loading progress: " + (async_load.progress * 100) + "%");
+            yield return null;
+        }
+    }
+}
