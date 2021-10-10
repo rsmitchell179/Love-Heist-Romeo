9e1b363a8652c182698c51ee813997aa1151d722
thealexanderkuang <49510751+thealexanderkuang@users.noreply.github.com>
2021-10-10 00:19:00 -0700

    Got the RC Art Gallery music to persist between the minigame and the art gallery

---
+++ No Hesitation Move!/Assets/Scripts/RCArtGalleryMusicScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RCArtGalleryMusicScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((get_scene_index() == 13) || (get_scene_index() == 19))
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    int get_scene_index()
+    {
+        int curr_scene_index = SceneManager.GetActiveScene().buildIndex;
+
+        return curr_scene_index;
+    }
+
+}
